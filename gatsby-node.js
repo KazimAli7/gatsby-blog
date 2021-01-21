2c1304056aaab67ca5305652c2317fd8fecd8a71
KazimAli7 <kazim.agha@invozone.com>
2021-01-21 13:19:56 +0500

    deploying changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,7 +41,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   paginate({
     createPage,
     items: result.data.swapi.getAllPost.result,
-    itemsPerPage: 20,
+    itemsPerPage: 10,
     pathPrefix: '/posts',
     component: path.resolve('src/templates/index.js')
   });
